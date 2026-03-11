fcb96a65931dd863e689750a510c698138c734c6
Roopam <rp246c6@gmail.com>
2026-03-11 13:58:13 +0530

    added files for spring data

--- src/main/java/com/codingnomads/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
+++ src/main/java/com/codingnomads/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
@@ -31,8 +31,21 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
             // nothing
         }
 
+        try {
+            // create course table using the JdbcTemplate method "execute"
+            jdbcTemplate.execute("CREATE TABLE courses (id BIGINT AUTO_INCREMENT PRIMARY KEY,"
+                    + "course_name VARCHAR(255) NOT NULL,instructor_name  VARCHAR(255) NOT NULL);");
+        } catch (Exception e) {
+            // nothing
+        }
+
         // create a list of first and last names
         List<Object[]> splitUpNames = Stream.of("Java Ninja", "Spring Guru", "Java Guru", "Spring Ninja")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // create a list of course and instructor names
+        List<Object[]> splitUpCourseNames = Stream.of("Java James", "Spring Ryan","Java Ryan", "Spring James")
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
 
@@ -40,6 +53,12 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
         for (Object[] name : splitUpNames) {
             jdbcTemplate.execute(
                     String.format("INSERT INTO employees(first_name, last_name) VALUES ('%s','%s')", name[0], name[1]));
+        }
+
+        // for each course & instructor name pair insert an Course into the database
+        for (Object[] name : splitUpCourseNames) {
+            jdbcTemplate.execute(
+                    String.format("INSERT INTO courses(course_name, instructor_name) VALUES ('%s','%s')", name[0], name[1]));
         }
 
         // query the database for Employees with first name Java
@@ -51,9 +70,24 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
                 // print each found employee to the console
                 .forEach(employee -> System.out.println(employee.toString()));
 
+        // query the database for Courses with course name Java
+        jdbcTemplate
+                .query(
+                        "SELECT id, course_name, instructor_name FROM courses WHERE course_name = 'Java'",
+                        (rs, rowNum) ->
+                                new Course(rs.getLong("id"), rs.getString("course_name"), rs.getString("instructor_name")))
+                // print each found course to the console
+                .forEach(course -> System.out.println(course.toString()));
+
         // truncate the table
         jdbcTemplate.execute("TRUNCATE TABLE employees;");
         // delete the table
         jdbcTemplate.execute("DROP TABLE employees");
+
+        // truncate the table
+        jdbcTemplate.execute("TRUNCATE TABLE courses;");
+        // delete the table
+        jdbcTemplate.execute("DROP TABLE courses");
+
     }
 }

24adbe1f1fee425ea066bd6ce53f34d683630034
Roopam <rp246c6@gmail.com>
2026-03-19 21:27:50 +0530

    added files for spring-data-jpa-repository-common-issues

--- src/main/java/com/codingnomads/springdata/example/dml/commonproblems/services/UserService.java
+++ src/main/java/com/codingnomads/springdata/example/dml/commonproblems/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class UserService {
 
     @Autowired

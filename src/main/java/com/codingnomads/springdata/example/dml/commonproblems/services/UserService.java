f213b999f1d5bee00e6a0510434859910aba9b75
Roopam <rp246c6@gmail.com>
2026-03-20 17:50:38 +0530

    Added file for spring-data-jpa-repository-common-issues

--- src/main/java/com/codingnomads/springdata/example/dml/commonproblems/services/UserService.java
+++ src/main/java/com/codingnomads/springdata/example/dml/commonproblems/services/UserService.java
@@ -7,6 +7,7 @@ import com.codingnomads.springdata.example.dml.commonproblems.models.User;
 import com.codingnomads.springdata.example.dml.commonproblems.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional

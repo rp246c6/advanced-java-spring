895a16dd558d38604c99aa3c3bf6ab03528d1dcc
Roopam <rp246c6@gmail.com>
2026-04-12 13:43:07 +0530

    spring-boot-tutorial-mvc-thymeleaf-part-2

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,22 @@
+/* Existing greeting function... */
 function greeting() {
     alert("Hello Spring Developer!!");
 }
 
+// New Function 1: Show Current Time
+function showTime() {
+    const now = new Date();
+    alert("Current Server Time: " + now.toLocaleTimeString());
+}
+
+// New Function 2: Toggle Dark Mode
+function toggleTheme() {
+    const body = document.body;
+    if (body.style.backgroundColor === "rgb(51, 51, 51)") {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    } else {
+        body.style.backgroundColor = "#333";
+        body.style.color = "#f4f4f4";
+    }
+}
